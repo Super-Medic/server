de5f86ce9a2a15821a58f7190ff0ec8c3780f467
Byounguk An <dbzl159753@naver.com>
2023-02-27 21:35:13 +0900

    login & join

--- routes/users/index.js
+++ routes/users/index.js
@@ -5,40 +5,50 @@ require("dotenv").config();
 
 
 /**
- * 회원가입
+ * 회원가입 입력
  */
 router.post('/join', (req, res) => {
   const info = {
-    "profile_nickname": req.body.profile_nickname,
-    "account_email": req.body.account_email,
-    "gender": req.body.gender,
-    "age_range": req.body.age_range,
-    "birthday": req.body.birthday
+    "name": req.body.name,
+    "email": req.body.account_email,
+    "phone_number": req.body.phone,
+    "telecom": req.body.telecom,
+    "birthday": req.body.birthday,
+    "gender": req.body.gender
   };
 
-  var sql = 'SELECT account_email FROM Users WHERE account_email=?'
-  mdbConn.dbSelect(sql, info['account_email'])
-  .then((rows) => {
-    if (!rows){
-      var sql = 'INSERT INTO Users(profile_nickname, account_email, gender, age_range, birthday) VALUES(?,?,?,?,?)';
-      var params = [info['profile_nickname'], info['account_email'], info['gender'], info['age_range'], info['birthday']];
-      
-      mdbConn.dbInsert(sql, params)
-      .then((rows) => {
-        if (!rows) res.status(500).send("false");
-        else res.status(200).send("true");
-      })
-      .catch((errMsg) => {
-        res.status(500).send(errMsg);
-      });
-    }
-    else res.status(200).send("true");
-  })
-  .catch((errMsg) => {
-    res.status(500).send(errMsg);
-  });
+  var sql = 'INSERT INTO Users(name, email, phone_number, telecom, birthday, gender) VALUES(?,?,?,?,?,?)';
+  var params = [info['name'], info['email'], info['phone_number'], info['telecom'], info['birthday'], info['gender']];
+
+  mdbConn.dbInsert(sql, params)
+    .then((rows) => {
+      if (!rows) res.status(500).send("false");
+      else res.status(200).send("true");
+    })
+    .catch((errMsg) => {
+      res.status(500).send(errMsg);
+    });
 });
-    
-  
+
+/**
+ * 회원가입 검증
+ */
+router.post('/joinverify', (req, res) => {
+  const info = {
+    "email": req.body.account_email,
+  };
+
+  var sql = 'SELECT email FROM Users WHERE email=?'
+  mdbConn.dbSelect(sql, info['account_email'])
+    .then((rows) => {
+      if (!rows) {
+        res.status(200).send("false");
+      }
+      else res.status(200).send("true");
+    })
+    .catch((errMsg) => {
+      res.status(500).send(errMsg);
+    });
+});
 
 module.exports = router;
